495fe09d6287e6c6e02ca1e33fd2a8f0051c272f
Seth Schallau <sethmschallau@gmail.com>
2018-12-09 18:04:36 -0500

    cleaned up the "menu" and added a while loop for a menu-like experience within the command line

--- mainMenu.py
+++ mainMenu.py
@@ -1,9 +1,18 @@
-from createNew import createTheDatabase
+from createNew import *
 import mysql.connector as mariadb
 import re
 
-print("1) Create a new Database ")
-
-inputChoice = raw_input()
-if inputChoice == "1":
-	createTheDatabase()
+#loop for the menu
+done = 0
+while done == 0:
+	print("1) Setup\n2) Add People\n3) Add Categories\n4) Spend Money\n5) Earn Money\n6) Exit")
+	inputChoice = raw_input()
+	#setup calls
+	if inputChoice == "1":
+		print("\n\n\n")
+		createTheDatabase()
+		createTables()
+		print("Setup Complete\n\n\n")
+	#exit
+	elif inputChoice == "6":
+		done = 1
