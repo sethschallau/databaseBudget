a886fa3f1cf900fd15c16225ddbc157d98acc016
Seth Schallau <sethmschallau@gmail.com>
2018-12-09 17:59:25 -0500

    added the createTables() function for setup

--- createNew.py
+++ createNew.py
@@ -3,12 +3,25 @@ import re
 def createTheDatabase():
 	mariadb_connection = mariadb.connect(user='budget', password='schallau49')
 	cursor = mariadb_connection.cursor()
-	#cursor.execute("CREATE DATABASE budget")
+	cursor.execute("CREATE DATABASE IF NOT EXISTS budget")
 	cursor.execute("SHOW DATABASES")
 	result = cursor.fetchall()
 	
 	for line in result:
-		print(line[0])
+		if line[0] == "budget":
+			print(line[0] + " database created")
 
+def createTables():
+	mariadb_connection = mariadb.connect(user='budget', password='schallau49', database='budget')
+	cursor = mariadb_connection.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS tbl_spending(amount DOUBLE null)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS tbl_saving(amount DOUBLE null)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS tbl_people(names TEXT null)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS tbl_category(type TEXT null)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS tbl_total(amount DOUBLE null)")
+
+	cursor.execute("SHOW TABLES")
+	result = cursor.fetchall()
 	
-	print("database created")
+	for line in result:
+		print(line[0] + " table created")
