e030216461c2a11ec12de4b03d83fb8a5f15c3e6
Seth Schallau <sethmschallau@gmail.com>
2018-12-18 14:47:08 -0500

    finished the spend income function. learned that .commit() is needed to save changes to tables, but not for creation of tables.

--- spendIncome.py
+++ spendIncome.py
@@ -5,15 +5,31 @@ from returnData import *
 def spendIncome():
 	mariadb_connection = mariadb.connect(user='budget', password='schallau49', database='budget')
 	cursor = mariadb_connection.cursor()
-	while choice != "n" || choice != "no":
+
+	proceed = "n", "no"
+	answer = "n"
+	while answer in proceed:
 		print("What category was the purchase out of the following?")
 		returnCategory()
+		print("\n")
 		category = raw_input()
 		print("What person was the purchase for out of the following?")
 		returnPeople()
+		print("\n")
 		person = raw_input()
 		print("How much? (xxx.xx format)")
-		amount = float(raw_input())
-		print("Is this correct?")
-		choice = raw_input()
+		cost = raw_input()
+		print("Is this correct? (y/n)")
+		answer = raw_input()
 
+	#send the cost to its corresponding tables. The person and category
+	#are both independently keeping track while totalSpent is used 
+	#to calculate and track all purchases.
+	cursor.execute("INSERT INTO per_"+person+"(amount) VALUES("+cost+")")
+
+	cursor.execute("INSERT INTO cat_"+category+"(amount) VALUES("+cost+")")
+
+	cursor.execute("INSERT INTO tbl_totalSpent(amount) VALUES("+cost+")")
+	#commit is needed to save these changes to the database.
+	mariadb_connection.commit()
+	print("\n")
